f8192268d42c5afdd4b5c3c18a4228e8f15627b4
muhamed shafiq <muhamed.ahmed.shafiq@gmail.com>
2025-10-24 18:25:28 +0300

    dev

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include "dev.h"
 #include "mul.h"
 #include "add.h"
 #include "subtract/subtract.h"
-#include "dev.h"
+
 int main()
 {
+    printf("%d",dev(8,5));
+
+
     printf("%d",mul(5,11));
 
     printf("%d",add(3,4));
 
     printf("%d",subtract(8,5));
-    printf("%d",dev(8,5));
+    
 }
